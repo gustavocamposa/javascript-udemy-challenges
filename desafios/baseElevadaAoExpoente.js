4fee9e1c0b22d1b2a6987f9926d05a9e996bf5fc
Gustavo Campos Alves <bryzynidy7@gmail.com>
2026-06-18 10:27:02 -0300

    feat/base-elevada-ao-expoente

---
+++ desafios/baseElevadaAoExpoente.js
@@ -0,0 +1,8 @@
+// 03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function elevarBase(base, expoente){
+    return base ** expoente 
+}
+
+console.log(elevarBase(4, 3))
+console.log(elevarBase(3, 3))
