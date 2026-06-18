1b26ffa548bd87b21a96a7fe2249cca05dabaae9
Gustavo Campos Alves <bryzynidy7@gmail.com>
2026-06-18 10:35:19 -0300

    feat/dividendo-divisor-resto

---
+++ desafios/dividendoEDivisor.js
@@ -0,0 +1,12 @@
+/* 04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+e o resto da divisão destes dois valores.
+*/
+
+function restoDosValores(dividendo, divisor){
+    console.log('O resultado é: ' + dividendo / divisor)
+    console.log('O resto é: ' + dividendo % divisor)
+}
+
+restoDosValores(4, 2)
+restoDosValores(14, 4)
+
