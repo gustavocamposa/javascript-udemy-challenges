c08895ac538b87070877bbcc9f6d5e06d5680b6c
Gustavo Campos Alves <bryzynidy7@gmail.com>
2026-06-19 10:48:56 -0300

    feat/num-extenso

---
+++ desafios/zeroADez.js
@@ -0,0 +1,43 @@
+/* 18) Faça um programa que leia um número entre 0 e 10, e escreva este número por extenso. Use o comando
+switch. Crie um case default que escreva ‘Número fora do intervalo.’
+*/
+
+let numero = 3
+
+switch (numero) {
+    case 0:
+        console.log('Zero')
+        break
+    case 1:
+        console.log('Um')
+        break
+    case 2:
+        console.log('Dois')
+        break
+    case 3:
+        console.log('Tres')
+        break
+    case 4:
+        console.log('Quatro')
+        break
+    case 5:
+        console.log('Cinco')
+        break
+    case 6:
+        console.log('Seis')
+        break
+    case 7:
+        console.log('Sete')
+        break
+    case 8:
+        console.log('Oito')
+        break
+    case 9:
+        console.log('Nove')
+        break
+    case 10:
+        console.log('Dez')
+        break
+    default:
+        console.log('Número fora da repetição')
+}
