3f3dc4283af3df7062fa9640de2b34690520d241
Gustavo Campos Alves <bryzynidy7@gmail.com>
2026-06-19 14:27:54 -0300

    feat/while

---
+++ desafios/helloWhile.js
@@ -0,0 +1,8 @@
+/*24) Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
+ */
+
+let i = 0
+while(i < 11){
+    i++
+    console.log('Hello world')
+}
