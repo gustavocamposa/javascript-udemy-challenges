5461e335ba130570937c2e9da4d46e33ba056b8b
Gustavo Campos Alves <bryzynidy7@gmail.com>
2026-06-19 14:36:00 -0300

    feat/pares

--- desafios/numWhile.js
+++ desafios/numWhile.js
@@ -4,4 +4,4 @@ let i = 0
 while(i < 50){
     i++
     console.log(i)
-}
+} 
---
+++ desafios/paresWhile.js
@@ -0,0 +1,7 @@
+//26) Fazer um programa para encontrar todos os pares entre 1 e 100.
+
+let i = 0
+while(i < 100){
+    i+=2
+    console.log(i)
+    }
