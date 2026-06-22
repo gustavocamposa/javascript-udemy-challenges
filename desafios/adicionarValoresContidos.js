8530cd3c63b70d812d96303f383eff3c16d2c19c
Gustavo Campos Alves <bryzynidy7@gmail.com>
2026-06-22 12:05:34 -0300

    feat/vetorPilha

---
+++ desafios/adicionarValoresContidos.js
@@ -0,0 +1,20 @@
+/*35) Crie dois vetores chamados vetorPilha e vetorAdiciona. Inicialmente, o vetorPilha conterá cinco elementos
+inteiros: [1, 2, 3, 4, 5]. Você deverá adicionar os valores contidos no vetorAdiciona [6, 7, 8, 9, 10] ao vetor pilha
+e mostrá-los no console. É importante lembrar que o método Push retorna somente o tamanho do Vetor. Ao
+final das operações imprima os vetores no console.
+ */
+
+function adicionarVetores(vetor1, vetor2){
+    for(let i = 0; i < vetor2.length; i++){
+        vetor1.push(vetor2[i])
+    }
+    return vetor1
+}
+
+const vetorPilha = [1, 2, 3, 4, 5]
+const vetorAdiciona = [6, 7, 8, 9, 10]
+
+adicionarVetores(vetorPilha, vetorAdiciona)
+
+console.log("Vetor pilha:", vetorPilha)
+console.log("Vetor adiciona:", vetorAdiciona)
